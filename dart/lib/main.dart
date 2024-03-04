da8e87d94c32a2dcb1adfae4080f3659bfbcde5c
André Lademann <alademann@thinkport.digital>
2024-03-04 20:07:00 +0100

    feat: Manuael span

--- dart/lib/main.dart
+++ dart/lib/main.dart
@@ -1,7 +1,43 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:opentelemetry/sdk.dart' as otel_sdk;
+
+// Optionally, multiple processors can be registered
+final provider = otel_sdk.TracerProviderBase(processors: [
+  otel_sdk.BatchSpanProcessor(
+    otel_sdk.CollectorExporter(
+      Uri.parse('http://localhost:4318/v1/traces'),
+    ),
+  ),
+  otel_sdk.SimpleSpanProcessor(otel_sdk.ConsoleExporter())
+]);
+
+final tracer = provider.getTracer('instrumentation-name');
+// final tracer = globalTracerProvider.getTracer('instrumentation-name');
+
+registerGlobalTracerProvider(provider) {}
 
 void main() {
+  final span = tracer.startSpan('doingWork');
   runApp(const MyApp());
+  // _manuelRequest();
+  span.end();
+}
+
+void _manuelRequest() async {
+  var client = http.Client();
+  try {
+    var response = await client.post(
+        Uri.https('https://requestbin.myworkato.com/1ea29g51', 'post'),
+        body: {'name': 'doodle', 'color': 'blue'});
+    var decodedResponse = jsonDecode(utf8.decode(response.bodyBytes)) as Map;
+    var uri = Uri.parse(decodedResponse['uri'] as String);
+    print(await client.get(uri));
+  } finally {
+    client.close();
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -13,21 +49,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -38,15 +59,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -59,50 +71,19 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
---
+++ melt/README.md
@@ -0,0 +1,5 @@
+
+
+```bash
+curl -i http://localhost:4318/v1/traces -X POST -H "Content-Type: application/json" -d @span.json
+```
